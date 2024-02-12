75c49a262f006f6d720289026e18a85d5c1ec688
Aisling-Mry <aislingmurray008@gmail.com>
2024-02-12 17:13:44 +0000

    reps generator operating for 2 separate exercises

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 
 repsExercises = ["squats", "lunges", "push ups", "donkey-kicks", "crunches", "world's greatest stretch"];
 
-TimedExercises = ["jumping jacks", "jog on the spot", "plank", "squat hold"]
+timedExercises = ["jumping jacks", "jog on the spot", "plank", "squat hold"]
 
-let repsGenerator = () => {
-    let i = Math.floor(Math.random() * repsExercises.length);
-    console.log(i)
-    let exercise1 = repsExercises[i];
-    return exercise1
+let generateRepIndex = () => {
+    index = Math.floor(Math.random() * repsExercises.length);
+    return index
 }
 
-console.log(repsGenerator())
+let repsGenerator = () => {
+    let exercise1 = repsExercises[generateRepIndex()];
+    console.log(exercise1);
+    let exercise2 = repsExercises[generateRepIndex()];
+      while (exercise1 === exercise2) {
+        exercise2 = repsExercises[generateRepIndex()]
+        } 
+      console.log(exercise2);
+    return [exercise1, exercise2]  
+   }
+    
+
+console.log(repsGenerator()[0])
 // Your circuit this week is __ reps of ____, __ reps of ____, and __ minutes of _____.
 // function 1 - output 2 non-identical reps exercises 
 // function 2 - output message calling function and implementing randon num gens.
