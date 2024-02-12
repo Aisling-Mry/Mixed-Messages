90fbb4a45f85cfa5ddaab9663d0d356d1c6fac65
Aisling-Mry <aislingmurray008@gmail.com>
2024-02-12 18:03:04 +0000

    completed prpject

--- main.js
+++ main.js
@@ -4,23 +4,27 @@ repsExercises = ["squats", "lunges", "push ups", "donkey-kicks", "crunches", "wo
 timedExercises = ["jumping jacks", "jog on the spot", "plank", "squat hold"]
 
 let generateRepIndex = () => {
-    index = Math.floor(Math.random() * repsExercises.length);
+    const index = Math.floor(Math.random() * repsExercises.length);
     return index
 }
 
 let repsGenerator = () => {
-    let exercise1 = repsExercises[generateRepIndex()];
-    console.log(exercise1);
+    const exercise1 = repsExercises[generateRepIndex()];
     let exercise2 = repsExercises[generateRepIndex()];
-      while (exercise1 === exercise2) {
-        exercise2 = repsExercises[generateRepIndex()]
-        } 
-      console.log(exercise2);
+        while (exercise1 === exercise2) {
+        exercise2 = repsExercises[generateRepIndex()];
+        }
     return [exercise1, exercise2]  
    }
-    
 
-console.log(repsGenerator()[0])
-// Your circuit this week is __ reps of ____, __ reps of ____, and __ minutes of _____.
-// function 1 - output 2 non-identical reps exercises 
-// function 2 - output message calling function and implementing randon num gens.
+
+function weeklyCircuit() {
+    let repCounter = () => Math.floor(Math.random() * 21);
+    let minuteCounter = () => Math.floor(Math.random() * 3 + 1);
+    let timedExerciseIndex = Math.floor(Math.random() * timedExercises.length);
+    let timedExercise = timedExercises[timedExerciseIndex];
+    let repExercises = repsGenerator()
+    console.log(`Your circuit this week is ${repCounter()} reps of ${repExercises[0]}, ${repCounter()} reps of ${repExercises[1]} and ${minuteCounter()} minute(s) of ${timedExercise}.`)
+}
+
+weeklyCircuit()
